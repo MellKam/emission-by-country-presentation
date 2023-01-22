95b1e04718092c889c9880f2a1db938771906720
Artem Melnyk <melkam464@gmail.com>
2023-01-22 21:08:32 +0100

    fix: install rmarkdown if not istalled

---
+++ README.md
@@ -0,0 +1,10 @@
+# Requirements
+
+- Rscript - 4.2.2
+- pandoc - 3.0
+
+# Build
+To build presentation run this command
+```bash
+Rscript build.R
+```
--- build.R
+++ build.R
@@ -1,3 +1,9 @@
+is_rmarkdown_installed <- require("rmarkdown")
+
+if (!is_rmarkdown_installed) {
+  install.packages("rmarkdown", repos = "https://cloud.r-project.org")
+}
+
 library(rmarkdown)
 
 rmarkdown::render(
