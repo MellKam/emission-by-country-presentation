41b0c5ac60d38294214c6c26252f16c49969943b
Artem Melnyk <melkam464@gmail.com>
2023-01-22 21:46:06 +0100

    configure theme with cleanrmd

--- build.R
+++ build.R
@@ -1,14 +1,20 @@
 is_rmarkdown_installed <- require("rmarkdown")
+is_cleanrmd_installed <- require("cleanrmd")
 
 if (!is_rmarkdown_installed) {
   install.packages("rmarkdown", repos = "https://cloud.r-project.org")
 }
+if (!is_cleanrmd_installed) {
+  install.packages("remotes", repos = "https://cloud.r-project.org")
+  remotes::install_github("gadenbuie/cleanrmd")
+}
 
 library(rmarkdown)
+library(cleanrmd)
 
 rmarkdown::render(
   "main.Rmd",
   output_dir = "build",
   output_file = "index.html",
-  rmarkdown::html_document()
+  cleanrmd::html_document_clean(theme = "stylize")
 )
